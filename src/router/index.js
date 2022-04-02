737ccfe871525e5a7eb64dc53d12bf0112d1e1a7
Tremendoc Dev <dev@tremendoc.com>
2022-04-02 21:04:43 +0100

    Pulling

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import { fetchCookie } from '../helpers/cookie';
+import { useSessionStorage } from '../hooks/useSessionStorage';
+import { getMerchantProfile } from '../services/users';
 import * as Pages from '../web/pages';
 import ConnectivityListener from './ConnectivityListener';
 
@@ -44,11 +46,49 @@ function Approuter({ history }) {
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+    console.log("Mounted Private...")
+    const [user, setUser] = useSessionStorage("user", {});
+    const [merchant, setMerchant] = useSessionStorage("merchant", {});
     const auth = {
         isAuthenticated: fetchCookie('1Q_SPA') ? true : false,
+    };
+      
+    if (auth.isAuthenticated && (!typeof user === 'object' || Object.keys(user).length <= 0) && (!typeof merchant === 'object' || Object.keys(merchant).length <= 0)){
+        console.log("Calling merchants....")
+        getMerchantProfile().then((re) => {
+            const { payload: { user: u, merchant: m} } = re;
+            setUser(u);
+            setMerchant(m);
+        }).catch(() => {
+            auth.isAuthenticated = false;
+            // removeCookie('_token')
+        });
     }
 
+    
+	// const rf = useCallback(() => {
+	// 	console.log("USE RF CALLBACK")
+	// 	if ((user && Object.keys(user).length <= 0) || (merchant && Object.keys(merchant).length <= 0)) {
+	// 		getMerchantProfile().then((res) => {
+	// 			if (res.status === HttpStatus.OK) {
+	// 				setUser(res.payload.user)
+	// 				setMerchant(res.payload.merchant)
+	// 			}
+	// 			// else Logout
+	// 		}).catch(() => {
+	// 			// Logout
+	// 		})
+	// 	}
+	// }, [merchant, user, setUser, setMerchant]);
+	
+	// useEffect(() => {
+	// 	console.log("USE RF")
+	// 	rf();
+	// }, [rf]);
+
     // Use getMerchant profile here in combination with useSessionstorage to persist user data
+
+    
 
     // if (auth.isAuthenticated && auth.isAuthorized === 'false') removeCookie('token')
     return (
